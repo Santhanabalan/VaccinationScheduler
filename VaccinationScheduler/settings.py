794cbc5a2a60a038e615db9eaa9375fc5d477cc1
Santhanabalan <bala2102002@gmail.com>
2023-06-19 19:16:42 +0530

    Apps: Add Accounts and Bookings App

--- VaccinationScheduler/settings.py
+++ VaccinationScheduler/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    "accounts.apps.AccountsConfig",
+    "bookings.apps.BookingsConfig",
 ]
 
 MIDDLEWARE = [
