0726802db69c8575712ad1f6fbd6339af844a1fe
Santhanabalan <bala2102002@gmail.com>
2023-06-20 19:45:28 +0530

    Bug: Input field is of type text for adding vaccination center timing
    -- Changed it to time input using forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,6 +5,10 @@ class VaccinationCenterForm(forms.ModelForm):
     class Meta:
         model = VaccinationCenter
         fields = ('name', 'address', 'from_time', 'to_time')
+        widgets = {
+            'from_time': forms.TimeInput(format='%H:%M',attrs={'type': 'time'}),
+            'to_time': forms.TimeInput(format='%H:%M',attrs={'type': 'time'}),
+        }
 
 class VaccinationSlotForm(forms.ModelForm):
     class Meta:
